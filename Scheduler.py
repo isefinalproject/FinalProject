479aff44454335a06ceb12ee67e1a84c0ae4ff22
Trent <Trent@dyn102085.cc.lehigh.edu>
2017-04-14 13:37:47 -0400

    first test

---
+++ Scheduler.py
@@ -0,0 +1,39 @@
+'''
+Created on Apr 14, 2017
+
+@author: Trent Insull
+'''
+
+class Scheduler:
+    
+    
+    """
+    V - set of vertices, key=ID, value=coordinates
+    E - edges (hashtable), key=edge, value="length" in minutes
+    Trucks - key=ID, value=Initial Location of a truck
+    """
+    def __init__(self, V, E, Trucks):
+        
+        
+        
+        
+        pass
+    
+    
+    def processNewOrders(self, newOrders):
+        pass 
+    
+    
+    
+    def updateLocationOfTrucks(self):
+        pass
+    
+    
+    """
+    for each truck create a file  history_truckId.log
+    which will save the travel history of each truck (also those which you haven't used)
+    """
+    def saveTravelHistoryOfAllTrucks(self):
+        
+        
+        pass
---
+++ Truck.py
@@ -0,0 +1,41 @@
+'''
+Created on Apr 14, 2017
+
+@author: Trent Insull
+'''
+class Truck:
+    
+    
+    def __init__(self, _id, initialLocation ):
+        self.id = _id
+        
+        
+        
+    """
+    Current location of a truck should be either at Node or on a "directed" edge
+    together with the information how far on this edge the truck is.
+    """    
+    def updateLocation(self):
+        pass 
+    
+    """
+         it should return the edge and also how far he is on this edge, e.g. [A,C],[5,30] 
+         if he is traveling 5 minutes on an edge A->C which takes 30 minutes to travel 
+    """
+    def getCurrentLocation(self):
+        pass
+    
+    
+    """
+    This should return total travel history, e.g.
+    A,C,30/30,P20;P94;P101    # this means he moved from A to C which was 30 minutes out of 30 minutes, he picked up 3 packages
+    C,D,30/40    # then he was traveling from C to D, only for 30 minutes (so he did not get to D)
+    D,C,30/40,P33;P34    # then he turn around and went back to C for 30 minutes and picked up 2 packages
+    C,E,50/50,D33;D20    # then he went to E and drop package 33 and 20
+    """
+    def getTotalTravelHistory(self):
+        history=[]
+        
+        
+        
+        return history
